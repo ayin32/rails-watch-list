d855d91aff2af8b33a653cf80a0deb7378cac2a9
Andy Yin <yin.andy32@gmail.com>
2023-02-23 12:12:38 +1100

    Root path test 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root_path "lists#index"
+  root "lists#index"
+
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
